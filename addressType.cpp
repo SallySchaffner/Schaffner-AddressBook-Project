c466de695a41596071aeaeb899f41e3c941fa85c
SallySchaffner <schaffners@vpcc.edu>
2024-01-24 12:26:53 -0500

    Constructor demonstration.

--- addressType.cpp
+++ addressType.cpp
@@ -1,1 +1,42 @@
 #include "addressType.h"
+#include <iostream>
+
+using namespace std;
+
+void addressType::print()
+{
+	cout << address << endl;
+	cout << city << ", " << state << " " << zipcode << endl;
+}
+
+
+// constructor with parameters
+addressType::addressType(string add, string cty, string st, int zip)
+{
+	address = add;
+	city = cty;
+	setState(st);
+	setZipcode(zip);
+}
+
+void addressType::setState(string st)
+{
+	if (st.length() > 2)
+	{
+		state = "XX";
+		cout << "State is invalid" << endl;
+	}	
+	else
+		state = st;
+}
+
+void addressType::setZipcode(int zip)
+{
+	if (zip <= 10000)
+	{
+		zip = 10000;
+		cout << "Zipcode is invalid" << endl;
+	}
+	else
+		zipcode = zip;
+}
--- addressType.h
+++ addressType.h
@@ -5,10 +5,32 @@ using namespace std;
 class addressType
 {
 private:
-	string address;
-	string city;
-	string state;
-	int zipcode;
+	string address = "";
+	string city = "";
+	string state = "XX";
+	int zipcode = 10000;
+
+public:
+	void print();
+
+	//preconditions - address and city are supplied, supply state as a valid postal abbrev.
+	//                supply zipcode as a 5-digit integer > 10000
+	// postcondition - use supplied values for address and city, validate state and zipcode
+	addressType(string="(Your address)", string="(Your city)", string="XX", int=10000);
+
+	// setState
+	// preconditions - supplied value is a vaild postal (2 character) state abbreviation
+	// postcondition - use the value supplied if valid, otherwise use the default ("XX")
+	void setState(string st);
+
+	//setZipcode
+	// precondtions - supplied is a 5 digit integer > 10000
+	// postcondition - use the supplied if valid, otherwise use the default (10000)
+	void setZipcode(int zip);
+
+	void setAddress(string add) { address = add;  }
+
+	string getState() { return state; }
 
 
 };
